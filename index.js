6b74bc40316b5db416483af1d1e83f645b1acad6
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-10-03 19:04:35 +0700

    add basic router req res

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('Server is Running');
+router.use('/create-blog', (req, res, next) => {
+    res.json({name: "Dede Saepulloh", email: "dedesaepulloh77@gmail.com"});
+    next();
 })
+
+router.get('/detail-blog', (req, res, next) => {
+    res.json({title: "Title Blog"});
+    next();
+})
+
+app.use('/', router);
+
 
 app.listen(4000);
