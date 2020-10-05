e89dda130493bab745853fa0a3bf63d9df1ceced
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-10-05 20:43:04 +0700

    create dummy endpoint response for register

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const productRoutes = require('./src/routes/products');
+const authRoutes = require('./src/routes/auth');
 
 app.use(bodyParser.json())
 
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/v1/customer', productRoutes);
+app.use('/v1/auth', authRoutes);
 
 
 app.listen(4000);
