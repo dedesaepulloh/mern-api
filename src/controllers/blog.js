56d5a1a2760bdb9d264a261f65ace57c86669ec0
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-10-12 21:19:43 +0700

    add update blog post

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,5 +1,6 @@
 const {validationResult} = require('express-validator');
 const BlogPost = require('../models/blog');
+const { post } = require('../routes/blog');
 
 
 exports.createBlogPost = (req, res, next) => {
@@ -77,4 +78,50 @@ exports.getBlogPostById = (req, res, next) => {
     .catch(err => {
         next(err);
     });
+}
+
+exports.updateBlogPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        const err = new Error('Invalid value');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
+    if(!req.file){
+        const err = new Error('Image harus diupload');
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then(post => {
+        if(!post){
+            const error = new Error('Blog Post Tidak Ditemukan');
+            error.errorStatus = 404;
+            throw error;
+        }
+
+        post.title = title;
+        post.body = body;
+        post.image = image;
+
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Update Success',
+            data: result
+        })
+    })
+    .catch(err => {
+        next(err);
+    })
 }
